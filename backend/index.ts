4fafb0d96c7caf838518ddb96fb0beb53ecd4342
andrejshylovich <andrejshylovich@gmail.com>
2025-12-20 19:11:44 +0300

    delete cors_options

--- backend/index.ts
+++ backend/index.ts
@@ -11,13 +11,7 @@ const app: Express = express();
 // --- Middleware ---
 app.use(express.json());
 
-const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL!] 
-    : ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 // --- Health check ---
