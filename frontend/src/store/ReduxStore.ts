b3e8bddbb4e7242944f77a9b13cc9d27a3dc8b14
andrejshylovich <andrejshylovich@gmail.com>
2026-03-17 15:28:56 +0300

    fix(store): correct 'authenticaion' typo in reducer name
    - Fix misspelling of 'authentication' in store configuration
    - Apply consistent code formatting with trailing commas

--- frontend/src/store/ReduxStore.ts
+++ frontend/src/store/ReduxStore.ts
@@ -1,16 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authenticaionReducer from './slices/AuthenticationSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import authenticationReducer from './slices/AuthenticationSlice'
 import modalReducer from './slices/ModalSlice'
 import bookReducer from './slices/BookSlice'
 
 export const store = configureStore({
-    reducer:{
-       authentication: authenticaionReducer,
-       modal: modalReducer,
-       book: bookReducer
-    }
+    reducer: {
+        authentication: authenticationReducer,
+        modal: modalReducer,
+        book: bookReducer,
+    },
 })
 
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
